d876b5c023849df82c66e571169da3ac5290293d
616164124@qq.com <ert123147>
2021-05-06 13:12:08 +0800

    2021年5月6日

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -3,12 +3,15 @@ import React, {Component} from 'react';
 
 class Top extends Component {
     render() {
+        let first = "nihao";
+
         return (
             <div>
-                <div>1111<br/></div>
+                <div>{first}<br/></div>
             </div>
         );
     }
+
 }
 
 export default Top;
