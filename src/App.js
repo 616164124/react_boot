9bda2b2dc3aa962678a99fbf10885779dcb71d03
hua <616164124@qq.com>
2021-05-09 21:44:05 +0800

    2021年5月9日

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Day01 from './components/Day01.jsx'
-
+import Day02 from './components/Day02/Day02.jsx'
 function App() {
     return (
         <div className="App">
             <Day01/>
             <div id="color"/>
+            <Day02/>
         </div>
     );
 }
--- src/components/Day02/Day02.jsx
+++ src/components/Day02/Day02.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
-
+import './Day02.css';
 export default class Day02 extends Component {
     render() {
         return (
             <div>
-                
+                <div id="css01" > hello</div>
             </div>
         )
     }
 }
+
+
