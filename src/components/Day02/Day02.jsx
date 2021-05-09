b2a679c9810cbcb7f7ed192805c46d4447d96002
hua <616164124@qq.com>
2021-05-09 21:27:08 +0800

    2021年5月9日

---
+++ src/components/Day02/Day02.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Day02 extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
