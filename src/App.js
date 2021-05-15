556e0eb3a626efc36b5037b7277270d0652f6258
hua <616164124@qq.com>
2021-05-15 16:03:24 +0800

    2021年5月15日

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Day01 from './components/Day01.jsx'
 import Day02 from './components/Day02/Day02.jsx'
+import Day03 from './components/Day03/Day03'
 function App() {
     return (
         <div className="App">
             <Day01/>
             <div id="color"/>
             <Day02/>
+            <div id="color"/>
+            <Day03/>
         </div>
     );
 }
